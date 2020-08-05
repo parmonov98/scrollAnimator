45299e6eefa225eff75372441566e5445f3deb58
Murod <parmonov98@yandex.ru>
2020-08-05 20:56:39 +0500

    screen resultion/ratio bug fixed. first release. :)

--- README.md
+++ README.md
@@ -2,51 +2,55 @@
  Page scrollAnimator in VanillaJS
 
 birinchi o'rinda animate.css ichida joylashgan odat ko'p ishlatiladigan effectlar uchun classlar yaratilgan. 
-fade-in, to-left, to-top, to-right, to-bottom, skew, pop-up, glitch
+fade-in, to-left, to-top, to-right, to-bottom, skew, pop-up, glitch, zoom-in
 
 Foydalanish:
 
-birinchi o'rinda kerakli animatsiyani biz elementga biriktiramiz.
-**animate** class standart class uni barcha scrollAnimator animatsiyasi ishlatiladigan elementlarga qo'shamiz.
-```HTML
-<div class="animate"></div>
-```
-endi esa **animate** class qo'shilgan elementga animatsiya nomini `data-animations="bu yerga"`  yozamiz.
+1. animatsiya bloklar(section) larni **data-threshold** attributi berib chiqamiz. 
+   **data-threshold**  qiymatini SETUP mode da **data-threshold** berilgan block elementlarni chap tepa qismida scroll qilishiz bilan ko'rinadi.
+
+2. **data-threshold** berib chiqilgan konteyner ichidagi elementlarga animatsiya berib chiqamiz.
+   
+
+  **animate** class standart class uni barcha scrollAnimator animatsiyasi ishlatiladigan elementlarga qo'shamiz.
+  ```HTML
+  <div class="animate"></div>
+  ```
+  endi esa **animate** class qo'shilgan elementga animatsiya nomini `data-animations="bu yerga"`  yozamiz.
 
 
-```HTML
-<div class="animate" data-animations="to-bottom"></div>
-```
-
-scroll bo'lishi bilan elementlar ekranda animatsiya bilan paydo bo'lishi uchun scroll ni window pageYOffset belgilab olib,  kerakli elementlarni ushlab turadigan section(parent = ota) elementga `data-offset="100"`   bu attributni yozib qo'ying.
-
-```HTML
-<div class="section" data-offset="100">
+  ```HTML
   <div class="animate" data-animations="to-bottom"></div>
-</div>
-```
+  ```
 
-Agar siz animatsiya vaqtini boshqarmoqchi bo'lsangiz unda, **data-animations** ga s1, s2, s3, s4, s5  qo'shib qo'ying. bo'sh joy bo'lishi kerak orasida.
-```HTML
-<div class="section" data-offset="100">
-  <div class="content animate" data-animations="to-bottom s2"></div>
-</div>
-```
-**content** klasli li div kerakli nuqtaga yetgandan keyin 2s kutib keyin animatsiya boshlanadi.
+  scroll bo'lishi bilan elementlar ekranda animatsiya bilan paydo bo'lishi uchun, animatsiya bo'lishi kerak bo'lgan section(blok) belgilab olib, kerakli elementlarni ushlab turadigan section(parent = ota) elementga `data-threshold="100"`   bu attributni yozib qo'ying.
+
+  ```HTML
+  <div class="section" data-threshold="100">
+    <div class="animate" data-animations="to-bottom"></div>
+  </div>
+  ```
+
+  Agar siz animatsiya vaqtini boshqarmoqchi bo'lsangiz unda, **data-animations** ga s1, s2, s3, s4, s5  qo'shib qo'ying. bo'sh joy bo'lishi kerak orasida.
+  ```HTML
+  <div class="section" data-threshold="100">
+    <div class="content animate" data-animations="to-bottom s2"></div>
+  </div>
+  ```
+  **content** klasli li div kerakli nuqtaga yetgandan keyin 2s kutib keyin animatsiya boshlanadi.
 
 
-va so'nggi qadam bu asosiy JS kutubxonani ulash.
-```HTML
+3. so'nggi qadam bu asosiy JS kutubxonani ulash.
+  ```HTML
 <script src="js/animate.js"></script>
 <script>
 	 const Animater = new animate(true);
 </script>
 ```
 
-**animate.js** faylini ulang va **animate**  obyektini hosil qiling.
+4. **animate**  obyektini hosil qiling.
 
 Demo fayl **demo** papkasida joylashgan.
-
 
 
 <h3>Misol uchun</h3>
@@ -54,7 +58,7 @@ Demo fayl **demo** papkasida joylashgan.
 <h4>HTML blokni belgilaymiz(data-offset orqali.) uni ichida o'sha nuqtaga yetkandan keyin animatsiya bo'ladigan elemenlarga animatsiya turini va vaqtini belgilaymiz.</h4>
 
 ```HTML
-<section class="sec4" data-offset="1300">
+<section class="sec4" data-threshold="1300">
     <h2 class="animate" data-animations="fade-in">section 4</h2>
     <div class="content animate" data-animations="fade-in s-2">content text</div>
 </section>
@@ -68,7 +72,7 @@ Demo fayl **demo** papkasida joylashgan.
 </script>
 ```
 
-#DEBUG(SETUP) mode ni yoqib kerakli animatsiyalarni sozlab olgandan keyin oddiy modega o'tkizing.
+`#DEBUG(SETUP) mode ni yoqib kerakli animatsiyalarni sozlab olgandan keyin oddiy modega o'tkizing.`
 
 
 Agar o'zingizni animatsiya effektlarizni qo'shmoqchi bo'lsangiz, marhamat, **src** papka ichidagi **animate.css** fayldagi effektlar qo'shib push request qiling. 
@@ -81,4 +85,4 @@ Proyekt uchun ishlatilgan texnlogiyalar:
 
 Ayni vaqtdagi qilinishi kerak ishlar:
 1. Animatsiya effektlarini yaratish/ko'paytirish.
-2. Turli xil o'lchamli ekranlarda ham bir xil natija olish yoki yondashuvni o'zgartirish orqali animatsiya ishga tushish vaqtini aniq sozlash sifatini oshirish.
+2. takliflar bering. elementlar ustida yana qanday boshqaruv bo'lishini xohlaysiz?
--- dist/animate.js
+++ dist/animate.js
@@ -9,99 +9,84 @@ class animate {
     this.setAllScreens();
     this.span = document.createElement('span');
     this.span.classList.add('pageYOffset');
-    this.span.setAttribute('title', `PXs value to animate a section/s after a user scrolls X PXs down.`);
-    this.span.appendChild(document.createTextNode(`scroll to see window.pageYOffset value`));
+    this.span.setAttribute('title', `You're in setup mode.`);
+    this.span.appendChild(document.createTextNode(`SETUP mode`));
     console.log(`Are you debugging?: ${this.DEBUG}`);
 
     if (this.DEBUG === true) {
       document.body.appendChild(this.span);
     }
-
-    window.addEventListener('scroll', e => {
-      if (this.DEBUG) {
-        // console.log(e);
-        // const test = document.querySelector(`[data-offset="${this.offsets[0]}"]`);
-        this.span.textContent = window.pageYOffset;
-      }
-
-      document.querySelector(`[data-offset="${this.offsets[0]}"]`);
-      let path = {};
-      path.pageY = window.scrollY || e.path[1].pageYOffset;
-
-      if (path) {
-        for (let i = 0; i < this.offsets.length; i++) {
-          const offset = this.offsets[i];
-
-          if (offset <= path.pageY) {
-            this.animateScreen(document.querySelector(`[data-offset="${this.offsets[i]}"]`));
-          }
-        }
-      }
-    });
   }
 
-  checkBrowser() {
-    let browsers = [{
-      name: opera,
-      isThis: false
-    }, {
-      name: firefox,
-      isThis: false
-    }, {
-      name: chrome,
-      isThis: false
-    }, {
-      name: IE,
-      isThis: false
-    }, {
-      name: Edge,
-      isThis: false
-    }, {
-      name: safari,
-      isThis: false
-    }]; // Chrome 1 - 71
+  buildThresholdList(element) {
+    let thresholds = [];
+    let elementHeight = element.getBoundingClientRect().height;
 
-    var isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
-    browsers[0].isThis = isChrome; // Opera 8.0+
+    for (var i = 1.0; i <= elementHeight; i++) {
+      var ratio = i / elementHeight;
+      thresholds.push(ratio);
+    }
 
-    var isOpera = !!window.opr && !!opr.addons || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
-    browsers[0].isThis = isOpera; // Firefox 1.0+
-
-    var isFirefox = typeof InstallTrigger !== 'undefined';
-    browsers[0].isThis = isFirefox; // Safari 3.0+ "[object HTMLElementConstructor]" 
-
-    var isSafari = /constructor/i.test(window.HTMLElement) || function (p) {
-      return p.toString() === "[object SafariRemoteNotification]";
-    }(!window['safari'] || typeof safari !== 'undefined' && safari.pushNotification);
-
-    browsers[0].isThis = isSafari; // Internet Explorer 6-11
-
-    var isIE =
-    /*@cc_on!@*/
-    false || !!document.documentMode;
-    browsers[0].isThis = isIE; // Edge 20+
-
-    var isEdge = !isIE && !!window.StyleMedia;
-    browsers[0].isThis = isEdge;
-    browsers.forEach(browser => {
-      if (browser.isThis) {
-        return browser.name;
-      } else return null;
-    });
+    return thresholds;
   }
 
   setAllScreens() {
-    this.screens = document.querySelectorAll("[data-offset]");
-    console.log(this.screens);
+    this.screens = document.querySelectorAll("[data-threshold]");
+    let options = {
+      root: null,
+      rootMargin: '0px'
+    };
+
+    let callback = (entries, observer) => {
+      entries.forEach(entry => {
+        console.log(entry);
+
+        if (this.DEBUG) {
+          entry.target.querySelector('.threshold').textContent = Math.floor(entry.intersectionRatio * 100);
+        } // if(!e.target.dataset.offset){
+        // }
+
+
+        if (entry.target.dataset.threshold) {
+          if (entry.isIntersecting === true && entry.target.dataset.threshold <= Math.floor(entry.intersectionRatio * 100)) {
+            console.log(entry.target.dataset.threshold);
+            console.log(entry.intersectionRatio * 100); // alert(1);
+
+            this.animateSection(entry.target);
+          }
+        } else {
+          // intersectionRatio, by default set to 0.5 * 100 % 
+          if (entry.isIntersecting === true && entry.isIntersecting >= 50) {
+            this.animateSection(entry.target);
+          }
+        }
+      });
+    };
+
+    this.screens.forEach(screen => {
+      options.threshold = this.buildThresholdList(screen);
+      this.observer = new IntersectionObserver(callback, options); // let target = document.querySelector('.sec8');
+
+      this.observer.observe(screen);
+    });
 
     if (this.DEBUG) {
       console.log(this.screens);
+      this.screens.forEach(screen => {
+        screen.style.position = 'relative'; // screen.style.position = 'opacity';
+
+        const threshold = document.createElement('span');
+        threshold.classList.add('threshold');
+        threshold.textContent = 0;
+        threshold.setAttribute('title', '% part of the element is visible');
+        screen.appendChild(threshold);
+      });
     }
 
     let attrs = [];
 
     for (let i = 0; i < this.screens.length; i++) {
-      attrs[i] = this.screens[i].getAttribute('data-offset');
+      attrs[i] = this.screens[i].getAttribute('data-threshold');
     }
 
     if (attrs.length === 0) {
@@ -112,18 +97,16 @@ class animate {
       alert.appendChild(document.createTextNode('You have not used any animation of scrollAnimator. You need to unlink the lib, if you don`t use it!'));
       document.body.appendChild(alert);
       document.querySelector('.modal-alert').classList.add('pop-up');
-    }
+    } // this.offsets = attrs;       
 
-    this.offsets = attrs;
   }
 
-  animateScreen(screen) {
+  animateSection(screen) {
     if (this.DEBUG) {
       console.log(screen);
     }
 
-    const elements = screen.querySelectorAll('[data-animations]');
-    console.log(elements);
+    const elements = screen.querySelectorAll('[data-animations]'); // console.log(elements);
 
     if (elements.length === 0) {
       // console.log(screen);            
